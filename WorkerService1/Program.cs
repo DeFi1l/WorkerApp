e851f458c403706864a2729180af895d750df79f
Денис Филлер <denis.filler@yandex.ru>
2023-04-21 21:55:37 +0300

    Add project files.

---
+++ WorkerService1/Program.cs
@@ -0,0 +1,10 @@
+using WorkerService1;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
